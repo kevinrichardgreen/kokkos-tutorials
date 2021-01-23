12563f93f8b0db7edfbb94c6d9af12de912aaedc
Christian Trott <crtrott@sandia.gov>
2021-01-22 16:55:51 -0800

    Add fence to mdrange solution to get proper timing

--- Exercises/mdrange/Solution/exercise_mdrange_solution.cpp
+++ Exercises/mdrange/Solution/exercise_mdrange_solution.cpp
@@ -123,6 +123,7 @@ int main( int argc, char* argv[] )
   // Timer products.
   struct timeval begin, end;
 
+  Kokkos::fence();
   gettimeofday( &begin, NULL );
 
   for ( int repeat = 0; repeat < nrepeat; repeat++ ) {
